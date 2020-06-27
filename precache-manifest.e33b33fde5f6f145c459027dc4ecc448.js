7c76e47f804acc81a546a8ab338aeec82a866d77
Thiago Filgueira <thiago.filgueira@efabrika.com.br>
2020-06-27 04:41:33 -0300

    Test

---
+++ precache-manifest.e33b33fde5f6f145c459027dc4ecc448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f83a6379892dfc098ff",
+    "url": "./wrtcoxt/static/js/runtime~main.fbfc0586.js"
+  },
+  {
+    "revision": "f50f6f64f50122bff154",
+    "url": "./wrtcoxt/static/js/main.33c406e9.chunk.js"
+  },
+  {
+    "revision": "8e66be725a39db9c884b",
+    "url": "./wrtcoxt/static/js/2.741f1e1b.chunk.js"
+  },
+  {
+    "revision": "f50f6f64f50122bff154",
+    "url": "./wrtcoxt/static/css/main.92a0066d.chunk.css"
+  },
+  {
+    "revision": "f209b11af9fdbfc66732aae3cd8b5f99",
+    "url": "./wrtcoxt/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f9ab3208e059688dcdf7a5302eb0429.js"
+  "./wrtcoxt/precache-manifest.e33b33fde5f6f145c459027dc4ecc448.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./wrtcoxt/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
