c1dc350b6e5d47fae8ff115540d6f0a9fdc3e35b
Thiago Filgueira <thiago.filgueira@efabrika.com.br>
2020-06-27 04:35:10 -0300

    Test

---
+++ precache-manifest.8f9ab3208e059688dcdf7a5302eb0429.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cf811f199a80b72acdad",
+    "url": "/static/js/main.f534bb34.chunk.js"
+  },
+  {
+    "revision": "7949b86f44407b266528",
+    "url": "/static/js/2.56ab3901.chunk.js"
+  },
+  {
+    "revision": "cf811f199a80b72acdad",
+    "url": "/static/css/main.92a0066d.chunk.css"
+  },
+  {
+    "revision": "0c8a4c93b88207cf35bcb5e439444019",
+    "url": "/index.html"
+  }
+];
