1d4969719d97644ceb047e77b05f839c0d624c83
Thiago Filgueira <thiago.filgueira@efabrika.com.br>
2020-06-27 05:18:04 -0300

    Test

---
+++ precache-manifest.62df1b164f33ce2c2b129bdb9f8c371e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1d709fa0909419f39660",
+    "url": "./static/js/main.0fccd508.chunk.js"
+  },
+  {
+    "revision": "6f92a192c862714b9dc4",
+    "url": "./static/js/2.cae7bc7d.chunk.js"
+  },
+  {
+    "revision": "1d709fa0909419f39660",
+    "url": "./static/css/main.92a0066d.chunk.css"
+  },
+  {
+    "revision": "d0d7af1c96411999a1297b102e5506cb",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.afc5a831394bb1a27d1eb1adeebe9a32.js"
+  "./precache-manifest.62df1b164f33ce2c2b129bdb9f8c371e.js"
 );
 
 workbox.clientsClaim();
